30785681dabc30823687bb427ccc89a98ade6f2c
Tim Joosten <Tjoosten3@gmail.com>
2017-06-26 14:07:54 +0200

    Implement empty language files

---
+++ resources/lang/fr/petition.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'first-heading' => '',
+
+    'intro' => '',
+
+    'second-heading' => '',
+    'third-heading' => '',
+
+    'text_1' => '',
+
+    'text_2' => '',
+    'text_3' => '',
+    'text_4' => '',
+
+    'fourth-heading' => '',
+
+    'point-1' => '',
+    'point-2' => '',
+    'point-3' => '',
+    'point-4' => "",
+    'point-5' => '',
+    'point-6' => '',
+    'point-7' => '',
+    'point-8' => '',
+    'point-9' => '',
+    'point-10' => '',
+    'point-11' => '',
+    'point-12' => '',
+    'point-13' => '',
+    'point-14' => '',
+
+    'fifth-heading' => '',
+
+    'point-15' => '',
+    'point-16' => '',
+    'point-17' => '',
+    'point-18' => '',
+    'point-19' => '',
+    'point-20' => '',
+    'point-21' => '',
+
+    'end-paragraph' => '',
+];
